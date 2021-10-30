ee6fd64e86a37fb025b7b22cf2ebd5261817150d
Nicolas Milicich <55760448+NicMi@users.noreply.github.com>
2021-10-30 18:56:05 +0000

    Version 2.1: JSON scoreboard You can now know how many times you won or lose. Code optimizations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import json
 # computer_input = random.randint(0,2)
 # print(computer_input)
 rock = '''
@@ -27,46 +28,35 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
 def saveResult(result, score):
     if(result == "You win"):
-        #guardar variable
-        score[0] = score[0] + 1
-        player = open("scoreplayer.py", "w")
-        player.write(str(score[0]))
-        player.close()
+        score["userScore"] = score["userScore"] + 1   
     if(result == "You lose"):
-        #guardar variable
-        score[1] = score[1] + 1
-        computer = open("scorecomputer.py","w")
-        computer.write(str(score[1]))
-        computer.close()
-
-def loadGame(score):
-    #load player score
-    try:
-        player = open("scoreplayer.py", "r")
-        score[0] = int(player.read())
-        player.close()
-    except:
-        score[0] = 0
+        score["computerScore"] = score["computerScore"] + 1
     
-    #load computer score
+    with open("scoreBoard.json", "w") as file:
+     json.dump(score,file, indent = 4)
+     
+def loadGame(score):
     try:
-        computer = open("scorecomputer.py", "r")
-        score[1] = int(computer.read())
-        computer.close()
+        with open("scoreBoard.json", "r") as file:
+           score = json.load(file)
     except:
-        score[1] = 0
-    print(f"The player won {score[0]} and the computer won {score[1]}")
-
+        score["userScore"] = 0
+        score["computerScore"] = 0
+    print(f"The player won {score['userScore']} and the computer won {score['computerScore']}")
 #first value-> player score
 #second value-> computer score
- 
-score = [0, 0]
+
+#Init 
+score = dict(userScore = 0, computerScore = 0)
 loadGame(score)
+
 playAgain = True
 while playAgain:
     humanPick = True
+    #check the input
     while humanPick:
         human_input = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
         if human_input < 0 or human_input > 2:
@@ -79,18 +69,22 @@ while playAgain:
     print(hands[human_input])
 
     computer_input = random.randint(0,2)
-
+    
+    #compare result
     print(f"Computer chose: {hands[computer_input]}")
     print(computer_input)
     choice_matrix = [ ["It\'s a draw", "You lose", "You win"], ["You win", "It\'s a draw", "You lose"], ["You lose", "You win", "It\'s a draw"] ]
     result = choice_matrix[human_input][computer_input]
     
     print(result)
-    saveResult(result, score)
+    if result != "It\'s a draw":
+        saveResult(result, score)
+    
+    #Check for play again
+    print(f"The player won {score['userScore']} and the computer won {score['computerScore']}")
     userChoice = input("Do you want to play again? type n or no to exit the program: ").lower()
     if userChoice == "n" or userChoice == "no":
         playAgain = False
-
 # human_input = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
 # computer_input = random.randint(0,2)
 # hands = [rock, paper, scissors]
